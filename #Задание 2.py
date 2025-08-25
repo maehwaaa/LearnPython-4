7f445e8ed29db3a785d673b79adf37ac70f8894a
maehwa <68327208+maehwaaa@users.noreply.github.com>
2025-08-25 18:20:23 +0500

    Create #Задание 2.py

---
+++ #Задание 2.py
@@ -0,0 +1,9 @@
+#Задание 2
+x = int(input("Введите число "))
+count = 0
+i = 1
+while i * i < x:
+    if x % i == 0: count += 2
+    i += 1
+if i * i == x: count += 1
+print("Количество делителей числа", x, "-", count)
